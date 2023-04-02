09c415fc387803ace2d231ebd1a6cf44e3a85d07
natalie <ntlshilin@gmail.com>
2023-04-02 13:56:45 -0400

    added a method for formatting a name

--- src/my_util/StringUtil.java
+++ src/my_util/StringUtil.java
@@ -17,4 +17,20 @@ public class StringUtil {
         return reversed;
     }
 
+    /**
+     * This method made by Loopcamp Batch#1
+     * This method accepts a String parameter and puts it in a format
+     * Ex:
+     * The format will be the following
+     * First letter is in upper case and the rest is in lower case
+     * Then returns a String in a formatted way*/
+
+    public static String fixFormat (String str) {
+
+        String fixed = str.trim();
+        fixed = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+        return fixed;
+
+    }
+
 }
