27a1899acb975995fc085aebac99debc5065770a
natalie <ntlshilin@gmail.com>
2023-05-13 15:03:54 -0400

    day42_work

---
+++ src/day42_collection/StackObjects.java
@@ -0,0 +1,45 @@
+package day42_collection;
+
+import java.util.Stack;
+
+public class StackObjects {
+    public static void main(String[] args) {
+        Stack <Character> stack = new Stack<>();
+        stack.push('a');
+        stack.push('b');
+        stack.push('c');
+        stack.push('d');
+        System.out.println(stack);
+
+        //Get whatever is on the Top of the stack
+        System.out.println("Top of the stack: " + stack.peek());
+
+        System.out.println("-----------------------");
+        //Remove the element at the top
+        stack.pop();
+        System.out.println(stack);
+        System.out.println("Top of the stack: " + stack.peek());
+
+        System.out.println("---------------");
+        stack.pop();
+        stack.pop();
+        System.out.println("Top of the stack: " + stack.peek());
+
+        System.out.println("---------------");
+        stack.push('z');
+        System.out.println("Top of the stack: " + stack.peek());
+
+        System.out.println("-----------------------");
+        //Can I remove the element from the beginning
+        //yes you can BUT then WE ARE NOT FOLLOWING THE IDEA THAT IS SPECIFIC TO STACK
+        stack.remove(0);
+        System.out.println("Top of the stack: " + stack.peek());
+
+        System.out.println("-----------------------");
+        stack.push('x');
+        System.out.println("Top of the stack: " + stack.peek());
+
+
+
+    }
+}
---
+++ src/day42_collection/StackObjects2.java
@@ -0,0 +1,15 @@
+package day42_collection;
+
+import java.util.Stack;
+
+public class StackObjects2 {
+    public static void main(String[] args) {
+        Stack <Integer> stack = new Stack<>();
+        stack.push(40);
+        stack.push(20);
+        stack.push(100);
+        stack.push(30);
+
+        System.out.println(stack);
+    }
+}
